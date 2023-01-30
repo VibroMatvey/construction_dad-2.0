b9b730d379e205c1cc5572409816938fed8bd37f
VibroMatvey <pooloff2004@gmail.com>
2023-01-30 17:25:17 +0400

    init

---
+++ resources/views/category/edit.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Редактировать категорию</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Главная</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="card col-3 mx-auto mt-2">
+                    <div class="card-body">
+                        <form action="{{route('category.update', $category->id)}}" method="POST">
+                            @method('patch')
+                            @csrf
+                            <div class="form-group">
+                                <label for="title">Наименование</label>
+                                <input name="title" type="text" value="{{$category->title}}" class="form-control"
+                                       id="title">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Редактировать</button>
+                        </form>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </div><!-- /.container-fluid -->
+    </section>
+@endsection
---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Категории</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <a href="{{ route('category.create') }}" class="float-right">
+                        <button class="btn btn-success"><i class="fas fa-plus"></i></button>
+                    </a>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body table-responsive p-0">
+                            <table class="table table-hover text-nowrap">
+                                <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Наименование</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($categories as $category)
+                                        <tr>
+                                            <td>{{ $category->id }}</td>
+                                            <td><a href="{{route('category.show', $category->id)}}">{{ $category->title }}</a></td>
+                                            <td class="d-flex">
+                                                <a href="{{route('category.edit', $category->id)}}">
+                                                    <button class="btn btn-primary">
+                                                        <i class="fas fa-pen"></i>
+                                                    </button>
+                                                </a>
+                                                <form action="{{route('category.delete', $category->id)}}" method="POST" class="ml-2">
+                                                    @csrf
+                                                    @method('delete')
+                                                    <button type="submit" class="btn btn-danger">
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+@endsection
